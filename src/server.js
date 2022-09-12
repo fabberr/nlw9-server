7311b9e6d3deb71e984188c49d04fcbd3113388d
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-09-12 02:01:52 -0300

    added basic server entrypoint

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+// configuring environment
+const HOSTNAME  = process.env.HOSTNAME || 'localhost';
+const PORT      = process.env.PORT || 3333;
+
+// create Express application
+const app = express();
+// app.use(express.json());
+
+// add routes
+app.get('/', (request, response) => {
+    return response.send('iwtcits');
+});
+
+// listen for requests on specified PORT (or 3333)
+app.listen(PORT, () => console.log(`[nlw9-server] Listening for connections on http://${HOSTNAME}:${PORT}\n`));
+
+// handle server shutdown signals
+const gracefulShutdown = () => {
+    console.log('[nlw9-server] Shutting down...\n');
+    // ... shutdown DB and other services...
+    process.exit(0);
+};
+
+process.on('SIGINT',    gracefulShutdown);  // interrupt
+process.on('SIGTERM',   gracefulShutdown);  // terminate
+process.on('SIGUSR2',   gracefulShutdown);  // sent by nodemon - terminate
