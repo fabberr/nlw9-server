3c59b04ae9c5b8cb6f1dd56928f443cd82b0de50
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-09-16 01:38:17 -0300

    added new API endpoints [api] added 'Fetch all games' endpoint at GET: `/games` [api] added 'Fetch ads by game' endpoint at GET: `/games/:id/ads`

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 // imports
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 // configuring environment
 const HOSTNAME  = process.env.HOSTNAME || 'localhost';
@@ -9,9 +10,50 @@ const PORT      = process.env.PORT || 3333;
 const app = express();
 app.use(express.json());
 
+// connect to DB using Prisma
+const prisma = new PrismaClient();
+
 // add routes
-app.get('/', (request: any, response: any) => {
-    return response.json('iwtcits');
+app.get('/games', async (request, response) => {
+
+    const games = await prisma.game.findMany({
+        select: {
+            id: true, name: true, coverURL: true, _count: { select: { ads: true } }
+        }
+    });
+
+    if (games.length) {
+        return response.status(200).json(games);
+    }
+    return response.status(204).json();
+});
+
+app.get('/games/:id/ads', async (request, response) => {
+
+    // extract params and make DB query
+    const gameId = request.params.id;
+    const ads = await prisma.ads.findMany({
+        select: {
+            id: true, gameId: true, name: true, yearsPlaying: true, weekdays: true, hourStart: true, hourEnd: true, useVoiceChat: true
+        },
+        where: {
+            gameId: gameId,
+            deleted: false
+        },
+        orderBy: { createdAt: 'desc' }
+    });
+
+    // check if any data was returned
+    if (ads.length) {
+        // return ad.weekdays comma-separated list as an array
+        return response.status(200).json(ads.map(ad => {
+            return {
+                ...ad,
+                weekdays: ad.weekdays.split(',')
+            };
+        }));
+    }
+    return response.status(204).json();
 });
 
 // listen for requests on specified PORT (or 3333)
